2ecd0d74bd0153561cc3b1e89e3dd1b8a920d687
nathira99 <nathirafarveen99@gmail.com>
2026-05-14 19:11:46 +0530

    Added Vercel frontend CORS

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      "https://invoice-generator-pi-sandy.vercel.app",
+  })
+);
 app.use(express.json());
 
 app.use('/api/invoices', invoiceRoutes);
